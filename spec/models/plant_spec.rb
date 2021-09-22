d58ae9d9d9fe2877e3afae76bb2dd9c14838a390
Michael Mattson <michael.p.mattson@gmail.com>
2021-09-22 09:28:11 -0600

    Add migrations

--- spec/models/plant_spec.rb
+++ spec/models/plant_spec.rb
@@ -1,8 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe Plot do
+RSpec.describe Plant, type: :model do
   describe 'relationships' do
-    it { should belong_to(:garden) }
     it { should have_many(:plot_plants) }
     it { should have_many(:plants).through(:plot_plants) }
   end
